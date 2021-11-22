5707fa67776d0a22cec386f9a0b35563b4936fc9
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2021-11-22 21:55:12 +0100

    Bruger kan oprettes

--- routes/brugere.js
+++ routes/brugere.js
@@ -1,23 +1,37 @@
 const { render } = require('ejs');
 const express = require('express');
+const bruger = require('../models/bruger');
 const router = express.Router();
-const bruger = require('../models/bruger')
+const Bruger = require('../models/bruger')
 
 //alle brugere//
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.render('brugere/index')
 });
 //alle brugere//
 
 //Nye bruger//
-router.get('/nye',(req, res) =>{
-    res.render('brugere/nye', {bruger: new bruger() })
+router.get('/nye',(req, res) => {
+    res.render('brugere/nye', { bruger: new Bruger() })
 });
 //Nye bruger//
 
 //Opret ny bruger//
-router.post('/',(req, res)=>{
-    res.send('Create')
+router.post('/',(req, res) => {
+    const bruger = new Bruger({ //hvorfor virker dette ikke?
+        name: req.body.name
+    })
+    bruger.save((err, nyBruger) => {
+        if (err){
+            res.render('brugere/nye',{
+                bruger: bruger,
+                errorMessage: 'Fejl i oprettelsen af bruger'
+            })
+        } else {
+            //res.redirect(`brugere/${nyBruger.id}`)
+            res.redirect('brugere')
+        }
+    })
 });
 
 
