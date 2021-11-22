8a8911323834f5402b7b9b1a7133a4a6bc1494bf
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2021-11-22 16:09:03 +0100

    Route til '/' som skal bruges til startside

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const PORT = 7070;
 
+const indexRouter = require('./routes/index')
+
+
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+app.use('/', indexRouter)
+
 app.listen(process.env.PORT || 7070)
+
+
