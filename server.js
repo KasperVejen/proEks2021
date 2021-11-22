6274ea34c9055d969f982a840ffde26de38be04c
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2021-11-22 17:05:33 +0100

    Rettelse af const db

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+//extentions //
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 //connection med database med MongoDB//
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
      useNewUrlParser: true
 });
-const db = mongoose.connect
+const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Der er forbindelse til Mongoose'))
-
 //connection med database med MongoDB//
 
+//extentions //
 
 const indexRouter = require('./routes/index')
 
-
+ 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
