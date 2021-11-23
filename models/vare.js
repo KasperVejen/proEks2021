da790a5c9cb7ecf2f09c18d2b9f198857909a895
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2021-11-23 14:19:29 +0100

    Model for en vare

--- models/vare.js
+++ models/vare.js
@@ -1,10 +1,35 @@
 const mongoose = require('mongoose');
 
-const brugerSkema = new mongoose.Schema({
-    name: {
+const vareSkema = new mongoose.Schema({
+    title: {
         type: String,
         required: true
-    }
+    },
+
+    beskrivelse: {
+        type: String
+    },
+
+    oprettelsesDato: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+
+    pris: {
+        type: Number,
+        required: true
+    },
+    vareBiled: {
+        type: String,
+        required: true
+    },
+    bruger: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'bruger'
+    },
+
 })
 
-module.exports = mongoose.model('bruger', brugerSkema);
+module.exports = mongoose.model('vare', vareSkema);
