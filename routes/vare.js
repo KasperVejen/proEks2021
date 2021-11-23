6139299f16f7ea2071cf0ce1285fa254e36e0b23
KasperVejen <kasper.ellegaard.vejen@gmail.com>
2021-11-23 14:27:07 +0100

    vare sider er oprettet og fungere

--- routes/vare.js
+++ routes/vare.js
@@ -4,21 +4,21 @@ const VareDB = require('../models/vare')
 
 //alle vare//
 router.get('/', async (req, res) => {
-    
+    res.send('alle vare')
 });
 //alle vare//
 
 
 //Nye vare//
 router.get('/nye',(req, res) => {
-
+    res.send('nye vare')
 });
 //Nye vare//
 
 
 //Opret ny vare//
 router.post('/', async (req, res) => {
-   
+   res.send('opret vare')
 });
 //Opret ny vare//
 
